b324b828a8d38e091ef4b59f9ff9763772e14783
agastya gaur <agastyagaur17@gmail.com>
2023-11-13 13:42:55 +0530

    add ui for registration and login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,53 @@
 import Layout from '../components/Layout'
-
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
 const Login = () => {
+
+    const [pass,setPass]= useState("")
+  const [email,setEmail]= useState("")
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    console.log(pass,email)
+    if( !pass || !email){
+        alert('please fill all the fields')
+        return
+    }
+    //api call
+
+}
   return (
-    <Layout title={"login page"}>login page</Layout>
+    <Layout title={"login page"}>
+        <div className=' h-[80vh] bg-gray-300 flex flex-col justify-center items-center'>
+        <div className=' bg-white px-16 pt-8 pb-12 mb-4'>
+              <h1 className=' text-3xl font-mono text-center'>Login</h1>
+              <form >
+             
+                <label  className=' block text-gray-600 mt-4'>Email: </label>
+                 <input  label="Email"
+                    type="text"
+                    id="email"
+                   value={email}
+                   onChange={(e)=>setEmail(e.target.value)}
+                   className=' w-full p-2 border border-gray-300 rounded-md focus:outline-none '
+                />
+                <label  className=' block text-gray-600 mt-4'>Password: </label>
+                 <input label="password"
+                    type="password"
+                    id="password"
+                   value={pass}
+                   onChange={(e)=>setPass(e.target.value)}
+                   className=' w-full p-2 border border-gray-300 rounded-md focus:outline-none '
+                />
+                
+                
+                <button onClick={(e)=>handleSubmit(e)} className=' bg-green-600   rounded-md p-2 mt-3   w-full px-4'>signin</button>
+                <p className=' inline-block'> Already have an account?</p>
+                <Link to="/login" className=' text-blue-500 ml-2'>Login</Link>
+              </form>
+        </div>
+    </div>
+
+    </Layout>
   )
 }
 
